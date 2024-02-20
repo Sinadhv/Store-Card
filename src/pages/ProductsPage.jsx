70f62ff972035ab445a81a31832bdf8e148ed378
sina dehvari <cnadhv7@gmail.com>
2024-02-20 18:00:39 +0330

    render products list

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,5 +1,6 @@
 import {useProducts} from "../context/ProductsContext";
 
+import styles from "./ProductsPage.module.css"
 
 
 function ProductsPage() {
@@ -7,8 +8,14 @@ function ProductsPage() {
  const products = useProducts();
  console.log(products);
   return (
-    <div>
-      <p>products</p>
+    <div className={styles.container}>
+     <div className={styles.products}>
+      {!products.length && <p>Loading...</p>}
+      {products.map((p) =>(
+       <p key={p.id}>{p.title}</p>
+      ))}
+     </div>
+     <div>Sidebar</div>
     </div>
   )
 }
