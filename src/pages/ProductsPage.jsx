8e1abc077d1b57cd333d5628118d551247c76d82
sina dehvari <cnadhv7@gmail.com>
2024-02-26 17:26:17 +0330

    create loader component
    install loader spinner library and create loader component

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,4 +1,5 @@
 import Card from "../components/Card";
+import Loader from "../components/Loader";
 import {useProducts} from "../context/ProductsContext";
 
 import styles from "./ProductsPage.module.css"
@@ -11,7 +12,7 @@ function ProductsPage() {
   return (
     <div className={styles.container}>
      <div className={styles.products}>
-      {!products.length && <p>Loading...</p>}
+      {!products.length && <Loader />}
       {products.map((p) =>(
        <Card key={p.id} data={p}/>
       ))}
