1144603abd4a1a559edf71747833d2e40afdbcdf
sina dehvari <cnadhv7@gmail.com>
2024-03-23 05:28:48 +0330

    change basket card

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,9 +1,20 @@
+import BasketCard from "../components/BasketCard";
+import { useCart } from "../context/CartContext"
 
 
 function CheckoutPage() {
+  const [state , dispatch] = useCart();
+
+  const clickHandler = ( type , payload) => {
+    dispatch({type , payload});
+  }
   return (
     <div>
-      <p>checkout</p>
+      <div>
+        {state.selectedItems.map((product) =>(
+          <BasketCard key={product.id} data={product} clickHandler={clickHandler}/>
+        ))}
+      </div>
     </div>
   )
 }
